78ffc50be7c0cfb3b43cb3c94dd5bd89e7a42947
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-18 18:52:30 +0530

    first numpy question

---
+++ 15_problem.py
@@ -0,0 +1,8 @@
+'''Write a program to create:
+A NumPy array of numbers from 1 to 10
+A NumPy array of all zeros with size 5'''
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9,10])
+zeros=np.zeros(5)
+print("number from 1-10:",arr)
+print("zeros:",zeros)
