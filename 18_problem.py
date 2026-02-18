67106508a421ca395c88dba82b69d9cf21ac56a7
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-18 19:23:56 +0530

    slicing

---
+++ 18_problem.py
@@ -0,0 +1,10 @@
+'''Given an array:
+[10, 20, 30, 40, 50, 60]
+First three elements
+Last two elements
+Elements from index 2 to 4'''
+import numpy as np
+arr_1d=np.array([10, 20, 30, 40, 50, 60])
+print("First three element:",arr_1d[0:3])
+print("Last two element:",arr_1d[4:6])
+print("Elements from index 2 to 4:",arr_1d[2:5])
