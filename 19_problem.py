29fe8ffdcf06af7b8c30f20a20c5ff25748f9598
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-19 20:02:47 +0530

    operation in numpy

---
+++ 19_problem.py
@@ -0,0 +1,7 @@
+'''create two array and find their addition multiplication and square'''
+import numpy as np
+arr1=np.array([1,2,3,4])
+arr2=np.array([5,6,7,8])
+print("Sum of two array:",arr1+arr2)
+print("Multiplication of two array:", arr1*arr2)
+print("Square of two array:", arr1**2)
