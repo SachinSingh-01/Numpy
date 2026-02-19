498bb170c6b00b816e856fa299dd840263f4ae86
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-19 20:31:31 +0530

    max, min, mean

---
+++ 20_problem.py
@@ -0,0 +1,10 @@
+'''Finding Minimum, Maximum, Mean
+[12, 45, 67, 23, 89]
+Maximum value
+Minimum value
+Mean value'''
+import numpy as np
+arr=np.array([12,45,67,23,89])
+print("Maximum number:",np.max(arr))
+print("Minimum number:",np.min(arr))
+print("Mean value:",np.mean(arr))
