1e5f203b96a14ab6a489a83a2ca63ffe935882eb
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-20 18:04:29 +0530

    corrected

--- 24_problem.py
+++ 24_problem.py
@@ -5,4 +5,11 @@ import numpy as np
 arr=np.array([[1,2,3],
              [4,5,6],
              [7,8,9]])
+print("Array:")
 print(arr)
+
+row_sums = arr.sum(axis=1)
+print("Sum of each row:", row_sums)
+
+col_sums = arr.sum(axis=0)
+print("Sum of each column:", col_sums)
