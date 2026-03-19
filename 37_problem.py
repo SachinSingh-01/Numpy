12875b49c819dec444f2be1c1127c6ab6dde7851
Sachin Singh <sachinsingh19012@gmail.com>
2026-03-19 18:56:47 +0530

    ravel

--- 37_problem.py
+++ 37_problem.py
@@ -828,9 +828,19 @@ in three separate steps.'''
 # print(entire_flip)
 
 '''78.Create a 1D array from 1 to 12 and reshape it into a 3x4 matrix.'''
+# import numpy as np
+# arr_1d=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+# print(arr_1d)
+# reshape=np.reshape(arr_1d,(3,4))
+# print("Reshape")
+# print(reshape)
+
+'''79.Flatten the matrix using .flatten(),.ravel().'''
 import numpy as np
 arr_1d=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 print(arr_1d)
-reshape=np.reshape(arr_1d,(3,4))
-print("Reshape")
-print(reshape)
+flatten=arr_1d.flatten()
+print(flatten)
+ravel=arr_1d.ravel()
+print(ravel)
+
